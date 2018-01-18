5a267ff4b68ea6381ed04e7726e4134917a2ecb5
toheaven <toheavener01@gmail.com>
2018-01-19 01:04:20 +0900

    To add retreiving jdbc settings from property file.

--- stock-value-common/src/dao/BaseDao.java
+++ stock-value-common/src/dao/BaseDao.java
@@ -1,7 +1,10 @@
 package dao;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.slf4j.Logger;
@@ -13,12 +16,27 @@ public class BaseDao {
 	static BasicDataSource ds = null;
 	
 	static {
+		Properties prop = new Properties();
+		InputStream is = null;
+		String host = "localhost";
+		String port = "5433";
+		String username = "postgres";
+		String password = "aaaa1111";
 		try {
+			URL url = ClassLoader.getSystemResource("database.properties");
+			if(url != null) {
+				is = url.openStream();
+				prop.load(is);
+				host = prop.getProperty("host");
+				port = prop.getProperty("port");
+				username = prop.getProperty("username");
+				password = prop.getProperty("password");
+			}
 			ds = new BasicDataSource();
 			ds.setDriverClassName("org.postgresql.Driver");
-	        ds.setUsername("postgres");
-	        ds.setPassword("aaaa1111");
-	        ds.setUrl("jdbc:postgresql://localhost:5433/stock");
+	        ds.setUsername(username);
+	        ds.setPassword(password);
+	        ds.setUrl("jdbc:postgresql://" + host + ":" + port + "/stock");
 	        ds.setInitialSize(2);
 	        ds.setConnectionProperties("charSet=UTF8");
 	        ds.setDefaultAutoCommit(true);
